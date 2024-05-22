d3ddca305f185feadb5fe9e77a25d5673379177b
JanisNied <122255186+JanisNied@users.noreply.github.com>
2024-05-22 14:00:31 +0300

    Smooth Drag and Drop

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -7,7 +7,7 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler,
     IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private RectTransform rTransform;
-
+    private bool running = false;
 
     void Start()
     {
@@ -21,28 +21,34 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler,
 
     public void OnBeginDrag(PointerEventData data)
     {
-        
+        running = true;
     }
 
     public void OnDrag(PointerEventData data)
-    {
-        Vector2 mousePosition =
-            new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-
-        mousePosition.x =
-            Mathf.Clamp(mousePosition.x, 0 + rTransform.rect.width / 2, 
-            Screen.width - rTransform.rect.width / 2);
-
-        mousePosition.y =
-            Mathf.Clamp(mousePosition.y, 0 + rTransform.rect.height / 2,
-            Screen.height - rTransform.rect.height / 2);
-
-        rTransform.position = mousePosition;
+    {    
     }
 
 
     public void OnEndDrag(PointerEventData data)
     {
-        
+        running = false;
     }
+    void FixedUpdate()
+    {
+        if (running)
+        {
+            Vector2 mousePosition =
+                new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            mousePosition.x =
+                Mathf.Clamp(mousePosition.x, 0 + rTransform.rect.width / 2,
+                Screen.width - rTransform.rect.width / 2);
+
+            mousePosition.y =
+                Mathf.Clamp(mousePosition.y, 0 + rTransform.rect.height / 2,
+                Screen.height - rTransform.rect.height / 2);
+
+            rTransform.position = Vector2.Lerp(rTransform.position, mousePosition, 0.1f);
+        }
+    }    
 }

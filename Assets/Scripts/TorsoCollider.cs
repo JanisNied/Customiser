43d0e8503ccc0092b2cf2151edab20614b2819e4
Janis <122255186+JanisNied@users.noreply.github.com>
2024-05-22 20:44:06 +0300

    Fixed Scaling Issues, removed animations

--- Assets/Scripts/TorsoCollider.cs
+++ Assets/Scripts/TorsoCollider.cs
@@ -18,7 +18,7 @@ public class TorsoCollider : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Shirt")
+        if (other.gameObject.CompareTag("Shirt"))
         {
             DestroyAllChildren(torsoAtt);
             DestroyAllChildren(leftarmAtt);

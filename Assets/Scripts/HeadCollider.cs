e8d4fd92c99d5f991462eb1bc2440a7a11dc3097
JanisNied <122255186+JanisNied@users.noreply.github.com>
2024-05-22 13:42:44 +0300

    Hitbox separation

---
+++ Assets/Scripts/HeadCollider.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadCollider : MonoBehaviour
+{
+    [Header("Attachments")]
+    [SerializeField] private GameObject headAtt;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Hat" || other.gameObject.tag == "Hair")
+        {
+            DestroyAllChildrenTagged(headAtt, other.gameObject.tag);
+            SingleObjectActivate objectholder = other.gameObject.GetComponent<SingleObjectActivate>();
+
+            GameObject newHat = Instantiate(objectholder.piece);
+            newHat.tag = other.gameObject.tag;
+            newHat.transform.SetParent(headAtt.transform, false);
+            Destroy(other.gameObject);
+        }
+    }
+    void DestroyAllChildrenTagged(GameObject obj, string tag)
+    {
+        foreach (Transform child in obj.transform)
+        {
+            if (child.gameObject.tag == tag)
+                Destroy(child.gameObject);
+        }
+    }
+}

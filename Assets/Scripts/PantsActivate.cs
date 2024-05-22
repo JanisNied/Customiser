a21b994bebe3d4ec45ad02b3a06b025aa1aa7d79
Janis <122255186+JanisNied@users.noreply.github.com>
2024-05-22 03:08:36 +0300

    More customisation options

---
+++ Assets/Scripts/PantsActivate.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PantsHolder : MonoBehaviour
+{
+    [SerializeField] public GameObject leftleg;
+    [SerializeField] public GameObject rightleg;
+    [SerializeField] private GameObject dragAndDropPrefab;
+    [SerializeField] private Sprite imageSprite;
+    [SerializeField] private Canvas canvas;
+
+    Vector2 objectPos = new Vector2(115.75f, 2.02f);
+
+    public void CreateObject()
+    {
+        GameObject GO = Instantiate(dragAndDropPrefab);
+        GO.transform.SetParent(canvas.transform, false);
+        GO.transform.localPosition = objectPos;
+        GO.tag = "Pants";
+        GO.name = "Pants_1";
+
+        GO.GetComponent<Image>().sprite = imageSprite;
+        PantsHolder newPants = GO.AddComponent<PantsHolder>();
+        newPants.leftleg = this.leftleg;
+        newPants.rightleg = this.rightleg;
+        newPants.dragAndDropPrefab = this.dragAndDropPrefab;
+        newPants.imageSprite = this.imageSprite;
+        newPants.canvas = this.canvas;
+        newPants.objectPos = this.objectPos;
+    }
+}

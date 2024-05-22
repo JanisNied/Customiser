c26fa066262fcd252556c6e216f7a26d51de461f
Janis <122255186+JanisNied@users.noreply.github.com>
2024-05-22 03:08:36 +0300

    More customisation options

--- Assets/Scripts/ClothingCollider.cs
+++ Assets/Scripts/ClothingCollider.cs
@@ -21,21 +21,20 @@ public class ClothingCollider : MonoBehaviour
     [SerializeField] private Material underwearmat;
 
     [Header("Body Parts with Underwear")]
-    [SerializeField] private GameObject torso;
-    [SerializeField] private GameObject leftleg;
-    [SerializeField] private GameObject rightleg;
+    [SerializeField] private GameObject torsoBody;
+    [SerializeField] private GameObject leftlegBody;
+    [SerializeField] private GameObject rightlegBody;
 
     void OnTriggerEnter2D(Collider2D other)
    {
         if (other.gameObject.tag == "Shirt")
         {
-
             DestroyAllChildren(torsoAtt);
             DestroyAllChildren(leftarmAtt);
             DestroyAllChildren(rightarmAtt);
             ShirtsHolder shirtsHolder = other.gameObject.GetComponent<ShirtsHolder>();
 
-            torso.GetComponent<Renderer>().material = skinmat;
+            torsoBody.GetComponent<Renderer>().material = skinmat;
 
             GameObject newTorso = Instantiate(shirtsHolder.torso);
             newTorso.transform.SetParent(torsoAtt.transform, false);
@@ -45,26 +44,48 @@ public class ClothingCollider : MonoBehaviour
 
             GameObject rightArm = Instantiate(shirtsHolder.rightarm);
             rightArm.transform.SetParent(rightarmAtt.transform, false);
-
-            Destroy(other.gameObject);
-
         }
-        if (other.gameObject.tag == "Hat")
+
+        if (other.gameObject.tag == "Hat" || other.gameObject.tag == "Hair")
         {
-            DestroyAllChildren(headAtt);
+            DestroyAllChildrenTagged(headAtt, other.gameObject.tag);
             SingleObjectActivate objectholder = other.gameObject.GetComponent<SingleObjectActivate>();
             
             GameObject newHat = Instantiate(objectholder.piece);
+            newHat.tag = other.gameObject.tag;
             newHat.transform.SetParent(headAtt.transform, false);
-
-            Destroy(other.gameObject);
         }
-   }
+
+        if (other.gameObject.tag == "Pants")
+        {
+            DestroyAllChildren(leftlegAtt);
+            DestroyAllChildren(rightlegAtt);
+            PantsHolder pantsHolder = other.gameObject.GetComponent<PantsHolder>();
+
+            leftlegBody.GetComponent<Renderer>().material = skinmat;
+            rightlegBody.GetComponent<Renderer>().material = skinmat;
+
+            GameObject leftleg = Instantiate(pantsHolder.leftleg);
+            leftleg.transform.SetParent(leftlegAtt.transform, false);
+
+            GameObject rightleg = Instantiate(pantsHolder.rightleg);
+            rightleg.transform.SetParent(rightlegAtt.transform, false);
+        }
+        Destroy(other.gameObject);
+    }
     void DestroyAllChildren(GameObject obj)
     {
         foreach (Transform child in obj.transform)
         {
             Destroy(child.gameObject);
+        }
+    }
+    void DestroyAllChildrenTagged(GameObject obj, string tag)
+    {
+        foreach (Transform child in obj.transform)
+        {
+            if (child.gameObject.tag == tag)
+                Destroy(child.gameObject);
         }
     }
     void swapSkin(string skin)
